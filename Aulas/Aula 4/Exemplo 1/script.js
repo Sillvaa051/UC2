e92360421c5e244b16ba2ed6139622cd953ab234
Sillvaa051 <joaossilva008@gmail.com>
2025-09-23 19:41:37 -0300

    adicionado a pasta aulas

---
+++ Aulas/Aula 4/Exemplo 1/script.js
@@ -0,0 +1,11 @@
+/* Exemplo
+     João Silva */
+      console.log ("ola mundo ")
+       prompt ("Qual é o seu nome?")
+
+       const nome= "João"
+       const idade= "16"
+       const sobrenome= "Silva"
+       const estudante= true
+       console.log ("O nome é", nome, "sua idade é", idade, ",o seu sobrenome é", sobrenome, ",você é um estudante?:", estudante==true)
+       
